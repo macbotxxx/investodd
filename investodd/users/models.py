55af627badd1fc1e232dc0eba1ec6ea5799e2936
macbotxxx <assanamamichael@gmail.com>
2021-09-22 22:36:27 +0100

    investodd setup

---
+++ investodd/users/models.py
@@ -0,0 +1,114 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from django.urls import reverse
+
+import uuid
+
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError(_('The given email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    """Default user for investodd."""
+    objects = UserManager()
+
+
+    id = models.UUIDField(
+        default = uuid.uuid4,
+        editable=False,
+        primary_key=True,
+        help_text=_("The unique identifier of the investor.")
+    )
+
+
+    #: First and last name do not cover name patterns around the globe
+    name = CharField(_("Name of User"), blank=True, max_length=255)
+
+    email = models.EmailField(
+        max_length=150,
+        null=True,
+        unique=True,
+        verbose_name=_("Email Address"),
+        help_text=_("The email address of the investor.")
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    username = None
+
+
+    first_name = models.CharField(
+        verbose_name=_("First names"),
+        max_length=50,
+        null=True,
+        help_text=_("The first nammes of the investor.")
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name=_("Last names"),
+        null=True,
+        help_text=_("The last nammes of the investor.")
+    )
+
+
+    profile_image = models.ImageField(
+        verbose_name=_("Profile Image"),
+        blank = True, null=True,
+        default = 'profile_image/default.png',
+        upload_to = 'profile_image/',
+        help_text=_("The profile image of the investor.")
+    )
+
+    class Meta:
+        verbose_name = _("Register Investor")
+        verbose_name_plural = _("Register Investor")
+
+    def __str__(self):
+        return str(self.email)
+
+
+    def get_absolute_url(self):
+        """
+        Get url for user's detail view.
+        Returns:
+            str: URL for user detail.
+        """
+        return reverse("users:detail", kwargs={"username": self.pk})
